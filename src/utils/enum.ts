83b644c07c3bf4b92b89fa3085807444e8398f98
Minamaher-sw <mosadefmena185@gmail.com>
2025-11-02 19:32:51 +0200

    Add enums for PeopleRole, UserRole, SubscriptionType, PaymentStatus, and PaymentMethod

---
+++ src/utils/enum.ts
@@ -0,0 +1,29 @@
+/* eslint-disable prettier/prettier */
+export enum PeopleRole {
+  ACTOR = 'ACTOR',
+  DIRECTOR = 'DIRECTOR',
+  WRITER = 'WRITER',
+  PRODUCER = 'PRODUCER',
+}
+export enum UserRole {
+  USER = 'USER',
+  ADMIN = 'ADMIN',
+}
+
+export enum SubscriptionType {
+  FREE = 'FREE',
+  BASIC = 'BASIC',
+  PREMIUM = 'PREMIUM',
+}
+
+export enum PaymentStatus {
+  PENDING = 'PENDING',
+  SUCCESS = 'SUCCESS',
+  FAILED = 'FAILED',
+}
+
+export enum PaymentMethod {
+  CARD = 'CARD',
+  PAYPAL = 'PAYPAL',
+  WALLET = 'WALLET',
+}
