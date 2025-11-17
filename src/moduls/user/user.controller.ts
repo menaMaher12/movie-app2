0b7677b4f1dc6f102139254fd95ca948a55456d7
Minamaher-sw <mosadefmena185@gmail.com>
2025-11-17 14:59:06 +0200

    fix: update import paths for consistency across modules and improve response DTOs

--- src/moduls/user/user.controller.ts
+++ src/moduls/user/user.controller.ts
@@ -1,5 +1,4 @@
 /* eslint-disable prettier/prettier */
-/* eslint-disable @typescript-eslint/require-await */
 /* eslint-disable prettier/prettier */
 /* eslint-disable prettier/prettier */
 import {
