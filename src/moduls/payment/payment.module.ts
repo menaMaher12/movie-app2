e2b4d50754989c0f17fe4c6b9fb45fe8d8d64af3
Minamaher-sw <mosadefmena185@gmail.com>
2025-11-24 15:52:59 +0200

    feat: enhance PaymentModule with additional imports and StripeService provider

--- src/moduls/payment/payment.module.ts
+++ src/moduls/payment/payment.module.ts
@@ -4,10 +4,16 @@ import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
 import { PaymentEntity } from './entity/payment.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from '../user/user.module';
+import { SubscriptionModule } from '../subscription/subscription.module';
+import { MailModule } from '../mail/mail.module';
+import { StripeService } from './stripe/stripe.service';
 
 @Module({
     controllers: [PaymentController],
-    providers: [PaymentService],
-    imports: [TypeOrmModule.forFeature([PaymentEntity])],
+    providers: [PaymentService,StripeService],
+    imports: [TypeOrmModule.forFeature([PaymentEntity]),JwtModule,ConfigModule,UserModule ,SubscriptionModule ,MailModule],
 })
 export class PaymentModule {}
