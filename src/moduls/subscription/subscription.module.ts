35d1c1fd259c3bec92b4c8480f73a04edf40e448
Minamaher-sw <mosadefmena185@gmail.com>
2025-11-24 16:11:07 +0200

    feat: update SubscriptionModule to include additional imports and exports

--- src/moduls/subscription/subscription.module.ts
+++ src/moduls/subscription/subscription.module.ts
@@ -1,12 +1,20 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { SubscriptionController } from './subscription.controller';
 import { SubscriptionService } from './subscription.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SubscriptionEntity } from './entity/subscription.entity';
+import { UserModule } from '../user/user.module';
+import { SubscriptionPlanModule } from '../subscription-plan/subscription-plan.module';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { MailModule } from '../mail/mail.module';
+import { CouponModule } from '../coupon/coupon.module';
 
 @Module({
   controllers: [SubscriptionController],
   providers: [SubscriptionService],
-  imports: [TypeOrmModule.forFeature([SubscriptionEntity])],
+  imports: [TypeOrmModule.forFeature([SubscriptionEntity]),UserModule,SubscriptionPlanModule,ConfigModule ,JwtModule,MailModule,CouponModule],
+  exports: [SubscriptionService],
 })
 export class SubscriptionModule {}
