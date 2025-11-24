18e7331b3f3fe69f30b0f8542397126e1115c736
Minamaher-sw <mosadefmena185@gmail.com>
2025-11-24 16:13:39 +0200

    feat: enhance UpdateSubscriptionDto to include is_active field with validation

--- src/moduls/subscription/dto/update-subscription.dto.ts
+++ src/moduls/subscription/dto/update-subscription.dto.ts
@@ -1,9 +1,20 @@
 /* eslint-disable prettier/prettier */
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateSubscriptionDto } from './create-subscription.dto';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsBoolean, IsOptional } from 'class-validator';
 
 /**
  * DTO for updating subscription details.
  * All fields are optional.
  */
-export class UpdateSubscriptionDto extends PartialType(CreateSubscriptionDto) {}
+export class UpdateSubscriptionDto extends PartialType(CreateSubscriptionDto) {
+    @ApiPropertyOptional({
+        example: true,
+        description: 'Indicates whether the subscription is active',
+        default: true,
+    })
+    @IsOptional()
+    @IsBoolean({ message: 'is_active must be a boolean value' })
+    is_active?: boolean;
+}
