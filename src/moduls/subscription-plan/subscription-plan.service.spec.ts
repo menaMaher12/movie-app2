6f602ad045bbdd1b023215cbd0b8dfb70a68346f
Minamaher-sw <mosadefmena185@gmail.com>
2025-11-24 16:00:03 +0200

    feat: add unit tests for SubscriptionPlanController and SubscriptionPlanService

---
+++ src/moduls/subscription-plan/subscription-plan.service.spec.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { Test, TestingModule } from '@nestjs/testing';
+import { SubscriptionPlanService } from './subscription-plan.service';
+
+describe('SubscriptionPlanService', () => {
+  let service: SubscriptionPlanService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [SubscriptionPlanService],
+    }).compile();
+
+    service = module.get<SubscriptionPlanService>(SubscriptionPlanService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
