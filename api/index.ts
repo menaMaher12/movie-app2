a616f15718e08c909546c3b6a79af388b1abb3c4
Minamaher-sw <mosadefmena185@gmail.com>
2025-11-30 23:57:01 +0200

    update

--- api/index.ts
+++ api/index.ts
@@ -9,13 +9,11 @@ import serverlessExpress from '@vendia/serverless-express';
 let server;
 
 export default async function handler(req, res) {
-    if (!server) {
-        const app = await createApp();
-        server = serverlessExpress({
-            app: app.getHttpAdapter().getInstance(),
-        });
-    }
-
-    return server(req, res);
+  if (!server) {
+    const app = await createApp();
+    server = serverlessExpress({ app: app.getHttpAdapter().getInstance() });
+  }
+  return server(req, res);
 }
+
 
