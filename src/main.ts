b53821f16a695ce10e79aa6f934bf8b621266073
Minamaher-sw <mosadefmena185@gmail.com>
2025-11-30 23:31:12 +0200

    update main to suitable vercel

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,78 @@
 /* eslint-disable prettier/prettier */
+// /* eslint-disable prettier/prettier */
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { NestExpressApplication } from '@nestjs/platform-express';
+// import cookieParser from 'cookie-parser';
+// import { QueryExceptionFilter } from './common/filters/query-exception.filter';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import helmet from 'helmet';
+// import bodyParser from 'body-parser';
+
+// import {
+//   initializeTransactionalContext,
+//   addTransactionalDataSource,
+// } from 'typeorm-transactional';
+// import { DataSource } from 'typeorm';
+
+// async function bootstrap() {
+
+//   initializeTransactionalContext();
+
+//   // ❗ Important: Disable built-in bodyParser
+//   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+//     bodyParser: false,
+//   });
+
+//   const dataSource = app.get(DataSource);
+//   addTransactionalDataSource(dataSource);
+
+//   app.setGlobalPrefix('api/v1',{
+//      exclude: ['/']
+//   });
+
+//   // Stripe raw body ONLY for webhook
+//   app.use('/api/v1/payment/webhook',
+//     bodyParser.raw({ type: 'application/json' }),
+//   );
+
+//   // Normal body parsing for the rest of the app
+//   app.use(bodyParser.json({ limit: '10mb' }));
+//   app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+
+//   app.use(cookieParser());
+//   app.use(helmet());
+
+//   app.enableCors({
+//     origin: [
+//       'http://localhost:3000',
+//       'http://localhost:3001',
+//       'http://localhost:4200',
+//       'https://movieapp-frontend-nine.vercel.app',
+//     ],
+//     credentials: true,
+//   });
+
+//   app.useGlobalFilters(new QueryExceptionFilter());
+
+//   const swagger = new DocumentBuilder()
+//     .setTitle('Movie App API')
+//     .setDescription('The Movie App API description')
+//     .setVersion('1.0')
+//     .build();
+
+//   const document = SwaggerModule.createDocument(app, swagger);
+//   SwaggerModule.setup('api/v1/swagger', app, document);
+
+//   const port = process.env.PORT ?? 3001;
+
+//   await app.listen(port, () => {
+//     console.log(`🚀 Server is running on http://localhost:${port}/api/v1`);
+//   });
+// }
+
+// bootstrap();
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
@@ -14,11 +88,10 @@ import {
 } from 'typeorm-transactional';
 import { DataSource } from 'typeorm';
 
-async function bootstrap() {
+export async function createApp() {
 
   initializeTransactionalContext();
 
-  // ❗ Important: Disable built-in bodyParser
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     bodyParser: false,
   });
@@ -26,16 +99,14 @@ async function bootstrap() {
   const dataSource = app.get(DataSource);
   addTransactionalDataSource(dataSource);
 
-  app.setGlobalPrefix('api/v1',{
-     exclude: ['/']
+  app.setGlobalPrefix('api/v1', {
+    exclude: ['/'],
   });
 
-  // Stripe raw body ONLY for webhook
   app.use('/api/v1/payment/webhook',
     bodyParser.raw({ type: 'application/json' }),
   );
 
-  // Normal body parsing for the rest of the app
   app.use(bodyParser.json({ limit: '10mb' }));
   app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 
@@ -63,11 +134,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swagger);
   SwaggerModule.setup('api/v1/swagger', app, document);
 
-  const port = process.env.PORT ?? 3001;
+  await app.init();
 
-  await app.listen(port, () => {
-    console.log(`🚀 Server is running on http://localhost:${port}/api/v1`);
-  });
+  return app;
 }
-
-bootstrap();
